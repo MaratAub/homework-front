da2adc608d0014293d78d35f62e3ca48b9dd3e33
MaratAub <marataub.511@gmail.com>
2025-12-18 22:17:10 +0300

    HW-3

--- src/HW/2/CurrentUser.tsx
+++ src/HW/2/CurrentUser.tsx
@@ -6,7 +6,7 @@ type CurrentUserPropsType = {
 
 export const CurrentUser = (props: CurrentUserPropsType) => {
   return (
-    <li key={props.user.id} id={`hw02-user-${props.user.id}`}>
+    <li id={`hw02-user-${props.user.id}`}>
       <strong>{props.user.name}</strong> (Age: {props.user.age})<strong> Address: </strong>
       {props.user.address.street}, {props.user.address.city}
     </li>
